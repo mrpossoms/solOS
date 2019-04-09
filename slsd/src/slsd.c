14b5daeb072d6f41753ea197c4b27ec51d82b00a
Kirk Roerig <mr.possoms@gmail.com>
2019-04-09 05:58:59 +0000

    working on slsd

---
+++ slsd/src/slsd.c
@@ -0,0 +1,12 @@
+#include <cfg.h>
+
+#include "constants.h"
+
+int main (int argc, char* argv[])
+{
+	cfg_base("/etc/solOS");
+
+	
+
+	return 0;
+}
---
+++ src/universe.h
@@ -0,0 +1,5 @@
+#ifndef __SOLOS_UNIVERSE_H
+#define __SOLOS_UNIVERSE_H
+
+#include "structs.h"
+#endif
